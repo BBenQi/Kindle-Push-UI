f2a0b6fc9287c797d369c4be0b913f4682a8c972
Dwoud <ynlbq@outlook.com>
2018-08-28 22:41:35 +0800

    完成push路由

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { PushComponent } from './push/push.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'push/:bookname', component: PushComponent }
 ];
 
 export const AppRoutes = RouterModule.forRoot(routes);
--- src/app/push/push.component.ts
+++ src/app/push/push.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-push',
   templateUrl: './push.component.html',
   styleUrls: ['./push.component.css']
 })
 export class PushComponent implements OnInit {
-
-  constructor() { }
+  public bookName: string;
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params) => {
+        this.bookName = params.bookname;
+      }
+    );
+
+    console.log(this.bookName);
   }
 
 }
